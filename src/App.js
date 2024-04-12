a837dcc4a2cbcc5453dc88713b6ddc412b6194fa
AvulaRudra <22695A0528@mits.ac.in>
2024-04-12 17:42:42 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import AddCourseForm from './components/AddCourseForm'
+import CourseList from './components/CoursesList'
+import AdminDashboard from './components/AdminDashboard'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AdminDashboard/>
+      <AddCourseForm/>
+      <CourseList/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
